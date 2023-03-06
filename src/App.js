d729969b99ac2b08d388f3a1d18a1ebda2d1e048
attemkinen <atte.makinen1@outlook.com>
2023-03-06 12:17:17 +0200

    added ag-grid and enabled sorting of columns

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import Todolist from './components/Todolist';
 
 import './App.css';
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,41 +1,60 @@
-import React, { useState } from 'react';
-import Todotable from './TodoTable';
+import React, { useState } from "react"
+import { AgGridReact } from 'ag-grid-react'
+import'ag-grid-community/styles/ag-grid.css'
+import 'ag-grid-community/styles/ag-theme-material.css';
+
+
 
 
 export default function Todolist() {
   const [todo, setTodo] = useState({
-    description: '',
-    date: ''
+    description: "",
+    date: "",
+    priority:''
   });
   const [todos, setTodos] = useState([]);
 
   const handleAddTodo = () => {
     setTodos([todo, ...todos]);
-    setTodo({description: '', date: ''});
-  }
+    setTodo({ description: "", date: "", priority: "" });
+  };
 
-  const handleDeleteTodo =(row) => {
-    setTodos(todos.filter((todo, index) => row !==index ));
+  const columns = [
+    {headername: 'Description', field: 'description', sortable: true },
+    {headername: 'Date', field: 'date', sortable: true},
+    {headername: 'Priority', field: 'priority', sortable: true},
+  ]
 
-  }
-
-  return(
+  return (
     <div>
-      
-      <h1>My Todos</h1>
+      <h1>Todolist</h1>
       <input
-        placeholder='Description'
+        placeholder="Description"
         value={todo.description}
-        onChange={e => setTodo({...todo, description: e.target.value})}
+        onChange={(e) => setTodo({ ...todo, description: e.target.value })}
       />
       <input
         type="date"
         value={todo.date}
-        onChange={e => setTodo({...todo, date: e.target.value})}
+        onChange={(e) => setTodo({ ...todo, date: e.target.value })}
       />
-      <button onClick={handleAddTodo}>Add Todo</button>
+       <input
+        placeholder="Priority"
+        value={todo.priority}
+        onChange={(e) => setTodo({ ...todo, priority: e.target.value })}
+      />
       
-     <Todotable todos = {todos}/>
+
+      <button onClick={handleAddTodo}>Add Todo</button>
+
+      <div className="ag-theme-material"
+        style={{height: '700px', width: '70%', margin: 'auto'}} >
+        <AgGridReact
+        columnDefs={columns}
+        rowData={todos}>
+        </AgGridReact>
+      </div>
+      
     </div>
   );
 }
