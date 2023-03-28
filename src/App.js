1f594f1fee7349a9bff7e6e8569a9fdd89dc8788
attemkinen <atte.makinen1@outlook.com>
2023-03-28 16:32:29 +0300

    created tab menu

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@
 import Todolist from './components/Todolist';
 
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+  } from "react-router-dom";
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <Todolist/>
       
+      <BrowserRouter>
+<Link to="/">Home</Link>{' '}
+<Link to="/todolist">Todolist</Link>{' '}
+
+<Routes>
+<Route exact path="/" element={<Home />} />
+<Route path="/todolist" element={<Todolist/>} />
+</Routes>
+</BrowserRouter>
+
     </div>
   );
 }
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,95 +1,92 @@
-import React, { useRef, useState } from "react"
-import { AgGridReact } from 'ag-grid-react'
-import'ag-grid-community/styles/ag-grid.css'
+import React, { useState, useRef } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-material.css';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+
 
 
 
 
 export default function Todolist() {
-  const [todo, setTodo] = useState({
-    description: "",
-    date: "",
-    priority:''
-  });
-  const [todos, setTodos] = useState([]);
-  const gridRef = useRef();
+    const [todo, setTodo] = useState({
+        description: '',
+        date: '',
+        priority: ''
+    });
 
-  const handleAddTodo = () => {
-    setTodos([todo, ...todos]);
-    setTodo({ description: "", date: "", priority: "" });
-  };
+    const [todos, setTodos] = useState([]);
 
-  const deleteTodo = () => {
-    if (gridRef.current.getSelectedNodes().length > 0) {
-    setTodos(todos.filter((todo, index) =>
-    index !== gridRef.current.getSelectedNodes()[0].childIndex))
+    const gridRef = useRef();
+
+
+    const columns = [
+        { field: "description", sortable: true, filter: true, floatingFilter: true },
+        { field: "date", sortable: true, filter: true, floatingFilter: true },
+        { field: "priority", sortable: true, filter: true, floatingFilter: true, cellStyle: params => params.value === "High" ? { color: 'red' } : { color: 'black' } }
+    ];
+
+
+
+    const addTodo = () => {
+        setTodos([todo, ...todos]);
+        setTodo({ description: '', date: '', priority: '' });
     }
-    else {
-    alert('No rows selected if you wish to continue SELECT ROW');
+
+    const deleteTodo = () => {
+        if (gridRef.current.getSelectedNodes().length > 0) {
+            setTodos(todos.filter((todo, index) =>
+                index !== gridRef.current.getSelectedNodes()[0].childIndex))
+        }
+        else {
+            alert('Select row first');
+        }
     }
-    }
+return (
+        <div>
+            
+            
+            <Stack direction="row" spacing={2} justifyContent="center" alignItems="center">
+                <TextField
+                    label="Description"
+                    variant="standard"
+                    name="desc" value={todo.description}
+                    onChange={e => setTodo({ ...todo, description: e.target.value })} />
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    {<DatePicker
+                        label="Date"
+                        format="DD-MM-YYYY"
+                        showTime={false}
+                        value={todo.date}
+                        onChange={date => setTodo({ ...todo, date })}
+                    />}
+                </LocalizationProvider>
+                <TextField
+                    label='Priority'
+                    variant="standard"
+                    name="priority" value={todo.priority}
+                    onChange={e => setTodo({ ...todo, priority: e.target.value })} />
+                <Button onClick={addTodo} variant="contained">Add</Button>
+                <Button onClick={deleteTodo} variant="contained">Delete</Button>
+            </Stack>
 
-  const columns = [
-    {headername: 'Description',
-     field: 'description',
-      sortable: true ,
-       filter: true, 
-       floatingFilter: true,
-        animateRows:true},
+            <div className="ag-theme-material"
+                style={{ height: '700px', width: '70%', margin: 'auto' }} >
+                <AgGridReact
+                    ref={gridRef}
+                    onGridReady={params => gridRef.current = params.api}
+                    rowSelection="single"
+                    columnDefs={columns}
+                    rowData={todos}
+                    animateRows={true}>
+                </AgGridReact>
+            </div>
 
-    {headername: 'Date',
-     field: 'date',
-      sortable: true,
-       filter: true,
-        floatingFilter: true,
-         animateRows:true},
-
-    {headername: 'Priority',
-     field: 'priority',
-      sortable: true,
-       filter: true,
-        floatingFilter: true,
-         animateRows:true,
-    cellStyle: params => params.value === "High" ? {color: 'red'} : {color: 'black'} 
-
-  },
-  ]
-
-  return (
-    <div>
-      <h1>Todolist</h1>
-      <input
-        placeholder="Description"
-        value={todo.description}
-        onChange={(e) => setTodo({ ...todo, description: e.target.value })}
-      />
-      <input
-        type="date"
-        value={todo.date}
-        onChange={(e) => setTodo({ ...todo, date: e.target.value })}
-      />
-       <input
-        placeholder="Priority"
-        value={todo.priority}
-        onChange={(e) => setTodo({ ...todo, priority: e.target.value })}
-      />
-      
-
-      <button onClick={handleAddTodo}>Add Todo</button>
-      <button onClick={deleteTodo}>Delete</button>
-
-      <div className="ag-theme-material"
-        style={{height: '700px', width: '70%', margin: 'auto'}} >
-        <AgGridReact
-        ref ={gridRef}
-        onGridReady={ params => gridRef.current = params.api }
-        rowSelection="single"
-        columnDefs={columns}
-        rowData={todos}>
-        </AgGridReact>
-      </div>
-      
-    </div>
-  );
+        </div>
+    );
 }
+
